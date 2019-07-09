0cd2eabd9ff0c84d84445526e15eb2ab0a1c4a5e
hajnalandor <hajnal.andor1993@gmail.com>
2019-07-09 10:53:13 +0200

    initial c

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,55 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+  <ul>
+  <li v-for="c in values">
+    {{ c }}
+    <p>joska</p>
+  </li>
+</ul>
+
+
   </div>
 </template>
 
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Fingerprint2 from 'fingerprintjs2'
 
+var comp
+
+
+
+if (window.requestIdleCallback) {
+    requestIdleCallback(function () {
+        Fingerprint2.get(function (components) {
+          console.log(components) // an array of components: {key: ..., value: ...}
+          comp = components
+        })
+    })
+} else {
+    setTimeout(function () {
+        Fingerprint2.get(function (components) {
+          console.log(components) // an array of components: {key: ..., value: ...}
+        comp = components
+        })  
+    }, 500)
+}
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  props: {
+    values: ["ANDOR","JOSKA"]
   }
 }
+
 </script>
+
 
 <style>
 #app {

b6ccfcce9d5c3b88358124c46bd59bdf02f988d4
hajnalandor <hajnal.andor1993@gmail.com>
2019-07-10 11:36:47 +0200

    adding custom fingerprint

--- src/App.vue
+++ src/App.vue
@@ -19,50 +19,20 @@ export default {
   name: 'app',
   created: function () {
     var client = new ClientJS();
-    console.log('fingerprint:  '+client.getFingerprint());
-    console.log('getEngine:  '+client.getEngine());
-    console.log('getSoftwareVersion:  '+client.getSoftwareVersion())
-    console.log('getBrowserData:  '+client.getBrowserData())
-    console.log('getUserAgent:  '+client.getUserAgent())
-    console.log('getBrowser:  '+client.getBrowser())
-    console.log('getBrowserVersion:  '+client.getBrowserVersion())
-    console.log('getBrowserMajorVersion:  '+client.getBrowserMajorVersion())
-    console.log('isChrome:  '+client.isChrome())
-    console.log('isFirefox:  '+client.isFirefox())
-    console.log('isSafari:  '+client.isSafari())
-    console.log('isMobileSafari:  '+client.isMobileSafari())
-    console.log('isOpera:  '+client.isOpera())
-    console.log('getOS:  '+client.getOS())
-    console.log('getOSVersion:  '+client.getOSVersion())
-    console.log('isWindows:  '+client.isWindows())
-    console.log('isMac:  '+client.isMac())
-    console.log('isLinux:  '+client.isLinux())
-    console.log('isUbuntu:  '+client.isUbuntu())
-    console.log('isSolaris:  '+client.isSolaris())
-    console.log('getDevice:  '+client.getDevice())
-    console.log('getDeviceType:  '+client.getDeviceType())
-    console.log('getDeviceVendor:  '+client.getDeviceVendor())
-    console.log('getCPU:  '+client.getCPU())
-    console.log('isMobile:  '+client.isMobile())
-    console.log('isMobileMajor:  '+client.isMobileMajor())
-    console.log('isMobileAndroid:  '+client.isMobileAndroid())
-    console.log('isMobileOpera:  '+client.isMobileOpera())
-    console.log('isMobileWindows:  '+client.isMobileWindows())
-    console.log('isMobileBlackBerry:  '+client.isMobileBlackBerry())
-    console.log('getScreenPrint:  '+client.getScreenPrint())
-    console.log('getColorDepth:  '+client.getColorDepth())
-    console.log('getCurrentResolution:  '+client.getCurrentResolution())
-    console.log('getAvailableResolution:  '+client.getAvailableResolution())
-    console.log('getDeviceXDPI:  '+client.getDeviceXDPI())
-    console.log('getDeviceYDPI:  '+client.getDeviceYDPI())
-    console.log('isJava:  '+client.isJava())
-    console.log('isFlash:  '+client.isFlash())
-    console.log('getFlashVersion:  '+client.getFlashVersion())
-    console.log('isSilverlight:  '+client.isSilverlight())
-    console.log('getTimeZone:  '+client.getTimeZone())
-    console.log('getLanguage:  '+client.getLanguage())
-    console.log('getSystemLanguage:  '+client.getSystemLanguage())
-   
+
+    var fingerPr = client.getFingerprint()+'-'
+    fingerPr += client.getCustomFingerprint(client.getTimeZone()) + '-'
+    fingerPr += client.getCustomFingerprint(client.getLanguage()) + '-'
+    fingerPr += client.getCustomFingerprint(client.getEngine(),client.getBrowser(),client.getBrowserVersion()) + '-'
+    fingerPr += client.getCustomFingerprint(client.getDevice(), client.getDeviceType())+ '-'
+    fingerPr += client.getCustomFingerprint(client.getOS,client.getOSVersion(),client.getCPU(),client.getSystemLanguage()) + '-'
+    fingerPr += client.getCustomFingerprint(client.isWindows(),client.isMac(), client.isLinux(),client.isUbuntu(),client.isSolaris()) + '-'
+    fingerPr += client.getCustomFingerprint(client.isChrome(),client.isSafari(),client.isMobileSafari(),client.isOpera()) + '-'
+    fingerPr += client.getCustomFingerprint(client.isMobile(),client.isMobileMajor(),client.isMobileAndroid(),client.isMobileOpera(),client.isMobileWindows(),client.isMobileBlackBerry()) + '-'
+    fingerPr += client.getCustomFingerprint(client.getScreenPrint()) + '-'
+    fingerPr += client.getCustomFingerprint(client.isJava(),client.isFlash(),client.getFlashVersion()) 
+    
+    console.log(fingerPr)
 
 
 

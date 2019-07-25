d661730be7d22947e818d03c7d9223cfad79f65b
hajnalandor <hajnal.andor1993@gmail.com>
2019-07-25 13:05:25 +0200

    finally finished

--- src/components/FingerPrint.vue
+++ src/components/FingerPrint.vue
@@ -1,18 +1,16 @@
 <script lang="ts">
-var start = new Date().getTime();
 import Vue from 'vue';
 import'clientjs';
 import Fingerprint2 from 'fingerprintjs2';
 
 const client = new ClientJS();
- var fingerPrintId = '';
- var fp = {};
- 
 export default Vue.extend({
   name: 'BrowserFinterprint',
   data () {    
     return {
-      tmp: ''
+      fingerPrintId: '',
+      fp: {}
+
     }
   },
   created() {
@@ -30,10 +28,10 @@ export default Vue.extend({
     getCustomFingerPr(fp2comp) {
       this.clientJsFp();
       this.fingerPrintjs2Fp(fp2comp);   
-      fingerPrintId = btoa(fingerPrintId)
+      this.fingerPrintId = btoa(this.fingerPrintId)
      },
     buildfp(fp2comp) {
-      fp['fingerPrintId'] = fingerPrintId;
+      this.fp['fingerPrintId'] = this.fingerPrintId;
       fp2comp.forEach((data) => {
         if (data.key == 'availableScreenResolution') {
           this.setAvailableScreenResolutin(data);
@@ -46,88 +44,88 @@ export default Vue.extend({
         } else if (data.key == 'webgl') {
           this.setWebgl(data);
         } else {
-          fp[data.key] = data.value;
+          this.fp[data.key] = data.value;
         }
       });
       this.setClientJsComponents();
-      console.log(fp)
+      console.log(this.fp)
     },
     clientJsFp() {
-      fingerPrintId = client.getFingerprint()+'-'
-      fingerPrintId += client.getCustomFingerprint(client.getTimeZone()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.getLanguage()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.getEngine(),client.getBrowser(),client.getBrowserVersion()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.getDevice(), client.getDeviceType())+ '-'
-      fingerPrintId += client.getCustomFingerprint(client.getCPU()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.getOS,client.getOSVersion(),client.getSystemLanguage()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.isWindows(),client.isMac(), client.isLinux(),client.isUbuntu(),client.isSolaris()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.isChrome(),client.isSafari(),client.isMobileSafari(),client.isOpera()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.isMobile(),client.isMobileMajor(),client.isMobileAndroid(),client.isMobileOpera(),client.isMobileWindows(),client.isMobileBlackBerry()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.getScreenPrint()) + '-'
-      fingerPrintId += client.getCustomFingerprint(client.isJava(),client.isFlash(),client.getFlashVersion()) + '-' 
+      this.fingerPrintId = client.getFingerprint()+'-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getTimeZone()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getLanguage()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getEngine(),client.getBrowser(),client.getBrowserVersion()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getDevice(), client.getDeviceType())+ '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getCPU()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getOS,client.getOSVersion(),client.getSystemLanguage()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.isWindows(),client.isMac(), client.isLinux(),client.isUbuntu(),client.isSolaris()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.isChrome(),client.isSafari(),client.isMobileSafari(),client.isOpera()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.isMobile(),client.isMobileMajor(),client.isMobileAndroid(),client.isMobileOpera(),client.isMobileWindows(),client.isMobileBlackBerry()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.getScreenPrint()) + '-'
+      this.fingerPrintId += client.getCustomFingerprint(client.isJava(),client.isFlash(),client.getFlashVersion()) + '-' 
     },
     fingerPrintjs2Fp(fp2comp) {
       for (var i = 0; i < fp2comp.length; i++) {
         if (Array.isArray(fp2comp[i].value)) {
         for (var j = 0; j < fp2comp[i].value.length; j++) {
-          fingerPrintId += client.getCustomFingerprint(fp2comp[i].value[j])
+          this.fingerPrintId += client.getCustomFingerprint(fp2comp[i].value[j])
           if (j < fp2comp[i].value.length - 1) {
-            fingerPrintId += '-'
+            this.fingerPrintId += '-'
           }
         }
         } else {
           if (fp2comp[i].key == 'adBlock') {
             continue;
           }
-          fingerPrintId += client.getCustomFingerprint(fp2comp[i].value) 
+          this.fingerPrintId += client.getCustomFingerprint(fp2comp[i].value) 
         }
         if (i < fp2comp.length - 1) {
-          fingerPrintId += '-'
+          this.fingerPrintId += '-'
         }
       }
     },
     setAvailableScreenResolutin(data) {
-      fp['availableScreenResolutionWidth'] = data.value[1];
-      fp['availableScreenResolutionHeight'] = data.value[0];
+      this.fp['availableScreenResolutionWidth'] = data.value[1];
+      this.fp['availableScreenResolutionHeight'] = data.value[0];
     },
     setScreenResolution(data) {
-      fp['screenResolutionWidth'] = data.value[1];
-      fp['screenResolutionHeight'] = data.value[0];
+      this.fp['screenResolutionWidth'] = data.value[1];
+      this.fp['screenResolutionHeight'] = data.value[0];
     },
     setCanvas(data) {
       var canvas = {};
       data.value.forEach((canvasElement) => {
         canvas[canvasElement.substring(0,canvasElement.indexOf(':'))] = canvasElement.substring(canvasElement.indexOf(':') + 1, canvasElement.length);
       });
-      fp['canvas'] = canvas;
+      this.fp['canvas'] = canvas;
     },
     setPlugins(data) {
       var plugins = [];
       data.value.forEach((element) => {
         plugins.push(element[0]);
       });
-      fp['plugins'] = plugins;
+      this.fp['plugins'] = plugins;
     },
     setWebgl(data) {
       data.value.forEach((element) => {
-        fp[element.substring(0,element.indexOf(':'))] = element.substring(element.indexOf(':') + 1, element.length); 
+        this.fp[element.substring(0,element.indexOf(':'))] = element.substring(element.indexOf(':') + 1, element.length); 
       });
     },
     setClientJsComponents() {
-      fp['currentResolution'] = client.getCurrentResolution();
-      fp['timeZoneAbbreviation'] = client.getTimeZone();
-      fp['engine'] = client.getEngine();
-      fp['engineVersion'] = client.getEngineVersion();
-      fp['cpu'] = client.getCPU();
-      fp['os'] = client.getOS();
-      fp['osVersion'] = client.getOSVersion();
-      fp['systemLanguage'] = client.getSystemLanguage();
-      fp['getSoftwareVersion'] = client.getSoftwareVersion();
-      fp['silverlight'] = client.isSilverlight();
-      fp['silverlightVersion'] = client.getSilverlightVersion();
-      fp['mimeType'] = client.getMimeTypes();
-      fp['ismimeType'] = client.isMimeTypes();
-      fp['canvasPrint'] = client.getCanvasPrint();
+      this.fp['currentResolution'] = client.getCurrentResolution();
+      this.fp['timeZoneAbbreviation'] = client.getTimeZone();
+      this.fp['engine'] = client.getEngine();
+      this.fp['engineVersion'] = client.getEngineVersion();
+      this.fp['cpu'] = client.getCPU();
+      this.fp['os'] = client.getOS();
+      this.fp['osVersion'] = client.getOSVersion();
+      this.fp['systemLanguage'] = client.getSystemLanguage();
+      this.fp['getSoftwareVersion'] = client.getSoftwareVersion();
+      this.fp['silverlight'] = client.isSilverlight();
+      this.fp['silverlightVersion'] = client.getSilverlightVersion();
+      this.fp['mimeType'] = client.getMimeTypes();
+      this.fp['ismimeType'] = client.isMimeTypes();
+      this.fp['canvasPrint'] = client.getCanvasPrint();
       this.setMobileInformations();
       this.setBrowserInformation();
       this.setDeviceInformation();
@@ -147,25 +145,25 @@ export default Vue.extend({
       mobile['isIpad'] = client.isIpad();
       mobile['isIpod'] = client.isIpod();
 
-      fp['mobile'] = mobile;
+      this.fp['mobile'] = mobile;
     },
     setBrowserInformation() {
-      fp['browserData'] = client.getBrowserData();
-      fp['BrowserMajorVersion'] = client.getBrowserMajorVersion();
-      fp['browser'] = client.getBrowser();
-      fp['browserVersion'] = client.getBrowserVersion();
+      this.fp['browserData'] = client.getBrowserData();
+      this.fp['BrowserMajorVersion'] = client.getBrowserMajorVersion();
+      this.fp['browser'] = client.getBrowser();
+      this.fp['browserVersion'] = client.getBrowserVersion();
     },
     setDeviceInformation() {
-      fp['device'] = client.getDevice();
-      fp['deviceType'] = client.getDeviceType();
-      fp['deviceVendor'] = client.getDeviceVendor();
-      fp['deviceType'] = client.getDeviceType();
+      this.fp['device'] = client.getDevice();
+      this.fp['deviceType'] = client.getDeviceType();
+      this.fp['deviceVendor'] = client.getDeviceVendor();
+      this.fp['deviceType'] = client.getDeviceType();
     },
     setFlashAndJava() {
-      fp['java'] = client.isJava();
-      fp['javaVersion'] = client.getJavaVersion();
-      fp['flash'] = client.isFlash();
-      fp['flashVersion'] = client.getFlashVersion();
+      this.fp['java'] = client.isJava();
+      this.fp['javaVersion'] = client.getJavaVersion();
+      this.fp['flash'] = client.isFlash();
+      this.fp['flashVersion'] = client.getFlashVersion();
     }
   } 
 })
